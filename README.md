106235b7e002a93563a2579be822853d3319667d
Leandro-Pinho <leandroessencia@gmail.com>
2024-06-05 19:28:41 -0300

    iniciando o readme

---
+++ README.md
@@ -0,0 +1,54 @@
+# API / Testes
+
+![](src/assets/image.png)
+
+---
+
+# Indice
+
+- [API / Testes](#api--testes)
+- [Indice](#indice)
+  - [📑 Sobre](#-sobre)
+  - [🚀 Tecnologias utilizadas](#-tecnologias-utilizadas)
+  - [📁 Como baixar o projeto](#-como-baixar-o-projeto)
+
+---
+
+## 📑 Sobre
+
+Este é o projeto de uma **API** para um ecommerce, desenvolvendo o model do banco de dados com cliente, produtos e pedidos e o relacionamento entre eles. Foi criado o CRUD para todas as tabelas, cliente, produtos e pedidos.
+
+---
+
+## 🚀 Tecnologias utilizadas
+
+o projeto foi desenvolvido utilizando as seguintes tecnologias
+
+- [node](https://nodejs.org/)
+- [express](https://expressjs.com/)
+- [mysql](https://www.mysql.com/)
+- [jest](https://jestjs.io/pt-BR/)
+- [typescript](https://www.typescriptlang.org/)
+
+---
+
+## 📁 Como baixar o projeto
+
+```bash
+
+# Clonar o repositório
+$ git clone https://github.com/Leandro-Pinho/desafio-tech-testes
+
+# Entrar no diretório
+$ cd server
+
+# Instalar as dependências
+$ yarn install
+
+# Iniciar o projeto
+$ yarn start
+
+
+```
+
+Desenvolvido ❤️ por Leandro Pinho
---
+++ src/module/aluno/__tests__/aluno.model.test.ts
@@ -0,0 +1,46 @@
+import { KnexService } from "../../../service/knex";
+import { Aluno } from "../aluno.model";
+
+describe("Aluno model Suite", () => {
+  it("deve retornar valores do modelo", async () => {
+    let knexServiceMock: any;
+
+    const knexMock = () => {
+      return {
+        select: jest.fn().mockReturnValueOnce([]),
+      };
+    };
+
+    knexServiceMock = {
+      obterConexao: jest.fn(() => knexMock),
+    };
+
+    const aluno = new Aluno(knexServiceMock);
+    const response = await aluno.getAll();
+    // expect(response).toBeTruthy();
+    expect(response.length).toBe(0);
+  });
+
+
+  it("deve salvar um aluno no modelo", async () => {
+    let knexServiceMock: any;
+
+    const knexMock = () => {
+      return {
+        insert: jest.fn().mockReturnValueOnce([100]),
+      };
+    };
+
+    knexServiceMock = {
+      obterConexao: jest.fn(() => knexMock),
+    };
+
+    const aluno = new Aluno(knexServiceMock);
+    const response = await aluno.store({
+        nome: 'Aluno teste',
+        cpf: 35685245
+    });
+    // expect(response).toBeTruthy();
+    expect(response).toEqual([100]);
+  });
+});
